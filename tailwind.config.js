89f68576fb15e4fc8d203daddeb315bf79d8495d
Ghama <ghamaalfajri@gmail.com>
2025-11-02 15:13:10 +0700

    Update tailwind
    Konfigurasi konten dan tema tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", 
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'crypto-yellow': '#FCD34D',
+        'crypto-dark': '#1A202C',
+        'crypto-light': '#FFFFFF',
+      }
+    },
   },
   plugins: [],
 }
-
